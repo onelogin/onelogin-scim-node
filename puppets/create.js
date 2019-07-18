2fa4fa48ba8a7cb3fc0350346cf754e2bb6d2362
Will Munslow <will@subterrane.com>
2019-07-17 17:20:37 -0700

    use Blake's code

--- puppets/create.js
+++ puppets/create.js
@@ -1,11 +1,12 @@
 const puppeteer = require("puppeteer");
 
-module.exports = async function() {
+module.exports = async function(user) {
   try {
     const browser = await puppeteer.launch({ headless: false, slowMo: 50 });
     const page = await browser.newPage();
-    await page.goto("https://subterrane.com");
-    await page.screenshot({ path: "subterrane.png" });
+
+    await doLogin(page, process.env.OREILLY_USERNAME, process.env.OREILLY_PASSWORD);
+    await doAddUser(page, user.givenName, user.familyName, user.email);
 
     await browser.close();
     return true;
@@ -13,3 +14,38 @@ module.exports = async function() {
     return false;
   }
 };
+
+async function doLogin(page, adminEmail, adminPassword) {
+  await page.goto('https://safaribooksonline.com/accounts/login/');
+  // Fill in username https://github.com/GoogleChrome/puppeteer/issues/441
+  //<input class="src-Input-input-field gridFullWidth--1yKPt" name="email" placeholder="Email Address or Username" value="">
+  await page.focus('input[name=email]');
+  await page.keyboard.type(adminEmail);
+  // Fill in password
+  await page.focus('input[name=password]');
+  await page.keyboard.type(adminPassword);
+  // Click
+  await page.click('button[type=button]');
+  // <button class="src-Button-button src-Button-primary src-Button-medium gridFullWidth--1yKPt" type="button">Sign In</button>
+  await page.waitForNavigation();
+}
+
+async function doAddUser(page, firstName, lastName, emailAddress) {
+  // Go to users page
+  await page.goto('https://learning.oreilly.com/manage/users');
+
+  // Wait until we can add a user. The first name field is fine.
+  await page.waitForSelector('input[name=first_name]');
+
+  await page.focus('input[name=first_name]');
+  await page.keyboard.type(firstName);
+  await page.focus('input[name=last_name]');
+  await page.keyboard.type(lastName);
+  await page.focus('input[name=email]');
+  await page.keyboard.type(emailAddress);
+
+  // Click "Add"
+  await page.click('button.add-button');
+
+  // Run away
+}
